e12e04e358691c7bf16144225d4fbdf005bab439
pooja <e0773178@u.nus.edu>
2025-01-20 09:55:14 +0800

    Add greet and exit functionality

--- src/main/java/Ella.java
+++ src/main/java/Ella.java
@@ -1,7 +1,24 @@
 public class Ella {
 
+    public static void printLines() {
+        int CONSOLE_WIDTH = 80;
+        String line = "─".repeat(CONSOLE_WIDTH);
+        System.out.println(line);
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Ella\nWhat can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
 
     public static void main(String[] args) {
-        
+        printLines();
+        greet();
+        printLines();
+        exit();
+        printLines();
     }
 }
