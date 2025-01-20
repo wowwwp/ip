d33cebb101a97a6159930387127c7b4a666b4dcb
pooja <e0773178@u.nus.edu>
2025-01-20 10:19:19 +0800

    Update greet and exit messages

--- src/main/java/Ella.java
+++ src/main/java/Ella.java
@@ -9,11 +9,11 @@ public class Ella {
     }
 
     public static void greet() {
-        System.out.println("Hello! I'm Ella\nWhat can I do for you?");
+        System.out.println("Hi! This is Ella\nWhat do you need? I've got this.");
     }
 
     public static void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye... I know you will come back soon!");
     }
 
     public static void main(String[] args) {
