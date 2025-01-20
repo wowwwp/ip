a9fa3d1ef34c0a738d105dc2afd7dd4b43525ede
pooja <e0773178@u.nus.edu>
2025-01-20 11:58:09 +0800

    Modify the output strings of certain methods for standardization

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,10 +9,18 @@ public class Task {
 
     public void markAsDone() {
         this.isDone = true;
+        System.out.println("Wow someone was productive\n");
+        System.out.println(this);
     }
 
     public void markAsUndone() {
         this.isDone = false;
+        System.out.println("Well i guess this is a future you problem\n");
+        System.out.println(this);
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getStatusIcon() {
