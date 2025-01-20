4e2cd8186529c9c1729fbe819de0171d66acaced
pooja <e0773178@u.nus.edu>
2025-01-20 13:29:25 +0800

    Implement todo, event, deadline command functionalities

--- src/main/java/Ella.java
+++ src/main/java/Ella.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ella {
 
     public static void printLines() {
         int CONSOLE_WIDTH = 80;
-        String line = "─".repeat(CONSOLE_WIDTH);
+        String line = "\u2500".repeat(CONSOLE_WIDTH);
         System.out.println(line);
     }
 
@@ -19,11 +20,12 @@ public class Ella {
 
     public static void process(Task task, ArrayList<Task> tasks) {
         tasks.add(task);
-        System.out.printf("added: %s\n", task.getDescription());
+        System.out.println("Ok, I will add this in...");
+        System.out.printf("%s%nYou have %d tasks in the list%n", task.toString(), tasks.size());
     }
 
     public static void printTasks(ArrayList<Task> tasks) {
-        System.out.println("This is what you got");
+        System.out.println("This is what you got:");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.printf("%d.%s%n", i + 1, tasks.get(i));
         }
@@ -32,6 +34,16 @@ public class Ella {
 
     public static Task getTask(String id, ArrayList<Task> tasks) {
         return tasks.get(Integer.parseInt(id) - 1);
+    }
+
+    public static String[] parseDeadline(String line) {
+        return line.split("/by");
+    }
+
+    public static String[] parseEvent(String line) {
+        String[] splitOne = line.split("/from");
+        String[] splitTwo = splitOne[1].split("/to");
+        return new String[]{splitOne[0], splitTwo[0], splitTwo[1]};
     }
 
     public static void main(String[] args) {
@@ -43,8 +55,9 @@ public class Ella {
 
         Scanner in = new Scanner(System.in);
         while(in.hasNextLine()) {
+            // parse command
             String line = in.nextLine();
-            String[] split = line.split(" ");
+            String[] split = line.split(" ", 2);
             String command = split[0];
 
             if (command.equals("bye")) {
@@ -52,6 +65,26 @@ public class Ella {
             }
 
             switch (command) {
+                case "todo":
+                    printLines();
+                    ToDo todo = new ToDo(split[1]);
+                    process(todo, tasks);
+                    printLines();
+                    break;
+                case "deadline":
+                    printLines();
+                    String[] parsedDeadline = parseDeadline(split[1]);
+                    Deadline deadline = new Deadline(parsedDeadline[0], parsedDeadline[1]);
+                    process(deadline, tasks);
+                    printLines();
+                    break;
+                case "event":
+                    printLines();
+                    String[] parsedEvent = parseEvent(split[1]);
+                    Event event = new Event(parsedEvent[0], parsedEvent[1], parsedEvent[2]);
+                    process(event, tasks);
+                    printLines();
+                    break;
                 case "mark":
                     Task taskDone = getTask(split[1], tasks);
                     printLines();
